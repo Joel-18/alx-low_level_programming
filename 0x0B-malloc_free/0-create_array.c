e17dff6afa6a72f737438a0e2f12b41eee577291
Joel Kudamo <kudamojoel@gmail.com>
2023-10-31 13:48:53 -0700

    create an array of characters and initialze it with a specific character

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -6,7 +6,7 @@
  * and initializes it with a specific char
  * @size: parameter that holds size of array
  * @c: character to be initilized
- * Return: ptArray
+ * Return: pointer to array or NULL if fail
  */
 
 char *create_array(unsigned int size, char c)
@@ -16,6 +16,10 @@ char *create_array(unsigned int size, char c)
 
 	/* allocate enough memory for all array members */
 	ptArray = malloc(size * sizeof(char));
+	if (size == 0 || ptArray == NULL)
+	{
+		return (NULL);
+	}
 
 	/* put char c in each of the memory locations */
 	for (count = 0 ; count < size ; count++)
