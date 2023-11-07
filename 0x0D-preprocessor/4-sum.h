537c0aa5f15d96fdbaf8208e4b44585fa93bd7b9
Joel Kudamo <kudamojoel@gmail.com>
2023-11-06 16:26:18 -0800

    compute the sum of two numbers using a function-like macro

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,24 @@
+#ifndef SUM_H
+#define SUM_H
+
+/**
+ * File - 4-sum
+ * @x: first input variable
+ * @y: second input variable
+ * Return: 0 (Success)
+ */
+
+#define SUM(x, y) ((x) + (y))
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
