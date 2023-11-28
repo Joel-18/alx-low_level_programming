80ee2b5b16e6ca9264667d03dab298f1195862d2
Joel Kudamo <kudamojoel@gmail.com>
2023-11-28 07:40:27 -0800

    read a text file and print it to the POSIX standard output

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,10 +2,11 @@
 #define MAIN_H
 
 #include <stdio.h>
-#include <stdarg.h>
-#include <string.h>
 #include <unistd.h>
-
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 
 int _putchar(char c);
